920acecde4af86ae389f045b361dec429b5155c4
FaisalA790 <161121763+FaisalA790@users.noreply.github.com>
2024-07-20 19:49:11 +0530

    app(2).py

--- app(2).py
+++ app(2).py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+i
 
 # Load the model, PCA, scaler, and kmeans using pickle
 with open('pipeline.pkl','rb') as file:
@@ -54,23 +53,7 @@ user_input_pca = pca.transform(user_input_scaled)
 cluster = kmeans.predict(user_input_pca)
 st.write(f'The customer belongs to cluster: {cluster[0]}')
 
-# Visualize clusters
-def plot_clusters(PCA_data, cluster_labels, user_input_pca, title):
-    plt.figure(figsize=(10, 6))
-    unique_labels = np.unique(cluster_labels)
-    palette = sns.color_palette("viridis", len(unique_labels))
-    
-    # Create a DataFrame for easy handling in seaborn
-    plot_data = pd.DataFrame(PCA_data, columns=['col1', 'col2', 'col3'])
-    plot_data['Cluster'] = cluster_labels
 
-    sns.scatterplot(x='col1', y='col2', hue='Cluster', palette=palette, data=plot_data, legend='full')
-    plt.scatter(user_input_pca[:, 0], user_input_pca[:, 1], c='red', marker='x', s=100)  # New customer point
-    plt.title(title)
-    plt.xlabel('PCA Component 1')
-    plt.ylabel('PCA Component 2')
-    plt.legend(title='Clusters')
-    st.pyplot(plt.gcf())
 
 # Load your original data (replace with your actual data)
 original_data = pd.read_excel('marketing_campaign1 (1).xlsx')
